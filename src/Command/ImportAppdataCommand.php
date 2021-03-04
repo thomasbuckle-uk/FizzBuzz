6ee51e1db4436e28be11ab967d1c1d136cee7190
Thomas Buckle <69787238+thomasbuckle-uk@users.noreply.github.com>
2021-03-04 08:48:07 +0000

    Added command for importing Appdata.ini

---
+++ src/Command/ImportAppdataCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class ImportAppdataCommand extends Command
+{
+    protected static $defaultName = 'app:import:appdata';
+    protected static string $defaultDescription = 'Import Appdata.ini file to database';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription(self::$defaultDescription)
+            ->addOption('dir', null, InputOption::VALUE_REQUIRED, 'Location of appdata.ini')
+            ->addOption(
+                'verbose',
+                null,
+                InputOption::VALUE_NONE,
+                'Display Output During Parsing',
+                '%kernel.project_dir%/tests/parse_test/appCodes.ini'
+            );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $arg1 = $input->getArgument('location');
+
+        if ($arg1) {
+            $io->note(sprintf('You passed an argument: %s', $arg1));
+        }
+
+        if ($input->getOption('option1')) {
+            // ...
+        }
+
+        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+
+        return Command::SUCCESS;
+    }
+}
