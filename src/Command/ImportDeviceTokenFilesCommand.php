1c0bca16a4bad840c62f6817811ae770192ea24d
Thomas Buckle <69787238+thomasbuckle-uk@users.noreply.github.com>
2021-03-04 12:19:06 +0000

    Added command for main import function of Device Entry Files

---
+++ src/Command/ImportDeviceTokenFilesCommand.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class ImportDeviceTokenFilesCommand extends Command
+{
+    protected static $defaultName = 'app:import:device-token-files';
+    protected static $defaultDescription = 'Add a short description for your command';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription(self::$defaultDescription)
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        return Command::SUCCESS;
+    }
+}
