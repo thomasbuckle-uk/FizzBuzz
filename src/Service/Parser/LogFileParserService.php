c1d1b449054c8b931c482813c3a5533e7f39cafb
Thomas Buckle <69787238+thomasbuckle-uk@users.noreply.github.com>
2021-03-05 08:29:58 +0000

    Created Parser Service, including LogFileParse that returns found files for given dir added symfony finder component bound  as service argument added sensioframeworkextra as project dependancy

---
+++ src/Service/Parser/LogFileParserService.php
@@ -0,0 +1,54 @@
+<?php
+
+
+namespace App\Service\Parser;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Finder\Finder;
+
+/**
+ * Class LogFileParserService
+ * @package App\Service
+ * */
+class LogFileParserService
+{
+    private LoggerInterface $logger;
+    private Finder $finder;
+    private string $projectDir;
+
+    public function __construct(LoggerInterface $logger, Finder $finder, string $projectDir)
+    {
+        $this->logger = $logger;
+        $this->finder = $finder;
+        $this->projectDir = $projectDir;
+    }
+
+    /**
+     * Returns Found Files
+     *
+     *
+     * @param string $depth The Depth Level expression - IE '>1' will start matching at level 1. Default = Recursive
+     * @param string $startLocation Starting Directory relative to %kernel.project_dir%
+     * @param string $fileType File type eg '.log' or '.csv' etc
+     * @return bool|Finder
+     */
+    public function getAllFilesByType(
+        string $depth,
+        string $startLocation = '/parser_test',
+        string $fileType = ".log"
+    ): bool|Finder {
+        ##Get new Finder object to force a reset, not sure if actually needed
+        ##Recursive toggle not set
+        $this->finder = new Finder();
+        $this->finder->files()->name('*' . $fileType)->ignoreUnreadableDirs()->in($this->projectDir . $startLocation);
+        if ($this->finder->hasResults()) {
+            return $this->finder;
+        }
+        return false;
+    }
+
+
+    public function parseColumns()
+    {
+    }
+}
