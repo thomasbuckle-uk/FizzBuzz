37e5c27bdc833379b391697421c8a328c9e9f848
Thomas Buckle <69787238+thomasbuckle-uk@users.noreply.github.com>
2021-03-05 09:22:04 +0000

    Added PHP_CodeSniffer Package Created Test class for LogFileParserService

--- src/Command/ImportAppdataCommand.php
+++ src/Command/ImportAppdataCommand.php
@@ -2,11 +2,9 @@
 
 namespace App\Command;
 
-use App\Service\DivisibleService;
 use App\Service\ImportService;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
--- src/Service/Parser/TagsParserService.php
+++ src/Service/Parser/TagsParserService.php
@@ -3,7 +3,6 @@
 
 namespace App\Service\Parser;
 
-
 class TagsParserService
 {
 }
